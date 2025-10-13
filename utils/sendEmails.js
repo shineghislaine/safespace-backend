84e0d791148bed57454e3d3bb1738fe0a58141fb
Ayow4 <chilko456@gmail.com>
2025-10-13 15:23:13 +0800

    yes

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (to, code) => {
   try {
     await resend.emails.send({
-      from: "Safespace <noreply@safespaceapp.com>", // You can customize this in Resend dashboard
+      from: "SafeSpace <onboarding@resend.dev>", // You can customize this in Resend dashboard
       to,
       subject: "SafeSpace Email Verification",
       html: `
